1ab45a8242cdd853a2cba83d6f49105e1f06e1eb
Sandeep Harip <sandeep.s.harip@in.ibm.com>
2015-09-16 18:29:07 +0530

    Add a button to send SMS Added a button in Layout In Activity , onCreate() , added a listner to Button to send a text message AndroidManifest with the required permission to send message

--- app/src/main/java/com/jd/san/templateapp1/MainActivity.java
+++ app/src/main/java/com/jd/san/templateapp1/MainActivity.java
@@ -3,11 +3,18 @@ package com.jd.san.templateapp1;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+    String tag="MyClass MainActivity :- ";
+    String mySMSMsg="Hi, This is Sandeep, Just saying Hello 123";
+    String mySMSTo="09527018833";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //Hello 123
+
+        setContentView(R.layout.activity_main);
+
+        final Button button =(Button) findViewById(R.id.btnSendSMS);
+        button.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v) {
+                Log.d(tag, "In the onClick() method, about to call sendSMS");
+                SmsManager sms = SmsManager.getDefault();
+                Log.d(tag, "In the onClick() method, sending SMS");
+                sms.sendTextMessage(mySMSTo, null, mySMSMsg, null, null);
+                Log.d(tag, "In the onClick() method, sent SMS to"+mySMSTo+" Msg is : "+mySMSMsg);
+
+            }
+        });
+        Log.d(tag, "In the onCreate() method, Finished setContentView");
     }
 
 
