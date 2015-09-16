afe296bc4b031dd337f70c68ba9aac37dfb9590c
Sandeep Harip <sandeep.s.harip@in.ibm.com>
2015-09-16 12:42:41 +0530

    First cut with all the files is now changed as follows a) changed to TargetSDK to 18 & Appcomat to 18 b) Renamed ActionBarActivity to Activity c) Removed Syle.xml and it's references

--- app/src/main/java/com/jd/san/templateapp1/MainActivity.java
+++ app/src/main/java/com/jd/san/templateapp1/MainActivity.java
@@ -1,12 +1,13 @@
 package com.jd.san.templateapp1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
